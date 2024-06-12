b23c8f22a86fc6d9cb95b80e4808506a26dd05d5
Kim Gang Yeon <rkddus3177@naver.com>
2024-06-12 15:15:48 +0900

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import Navbar from "./layout/Navbar/Navbar";
+import Footer from "./layout/Footer/Footer";
+
+function Layout() {
+  return (
+    <div className="flex flex-col h-screen justify-between">
+      <Navbar />
+      <main className="mb-auto w-10/12 max-w-4xl mx-auto">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">hello</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LandingPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
